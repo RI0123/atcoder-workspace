139c9985366b9d2a3ff038bbb8d638893380063f
RI0123 <ishiura.ryusei@icloud.com>
2026-06-07 22:10:50 +0900

    Solved: abs_abc049c at 2026-06-07 22:10

--- abs/abc049c/main.cpp
+++ abs/abc049c/main.cpp
@@ -1,9 +1,18 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-using ll = long long;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int main() {
-    // Code here
-    return 0;
+    string s; cin >> s;
+    int l = s.size();
+
+    while (l > 0) {
+        if (l >= 7 && s.substr(l-7, 7) == "dreamer") l -= 7;
+        else if (l >= 6 && s.substr(l-6, 6) == "eraser") l -= 6;
+        else if (l >= 5 && s.substr(l-5, 5) == "dream") l -= 5;
+        else if (l >= 5 && s.substr(l-5, 5) == "erase") l -= 5;
+        else break;
+    }
+
+    if (l == 0) cout << "YES\n";
+    else cout << "NO\n";
 }
