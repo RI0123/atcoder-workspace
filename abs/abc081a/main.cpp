a506d147e38e646119333f1fae6a77a18b3cd0a6
RI0123 <ishiura.ryusei@icloud.com>
2026-06-07 23:31:41 +0900

    Solved: abs_abc081a at 2026-06-07 23:31

--- abs/abc081a/main.cpp
+++ abs/abc081a/main.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int main() {
-    // Code here
-    return 0;
+    string origin;
+    cin >> origin;
+    int count = 0;
+
+    rep(i, origin.size()) {
+        if (origin.at(i) == '1') {
+            count++;
+        }
+    }
+    cout << count << endl;
 }
