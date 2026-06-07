2454f864f5ab6e3b1c0312751edf4d9b40c7d7d8
RI0123 <ishiura.ryusei@icloud.com>
2026-06-08 00:27:41 +0900

    Solved: abs_abc081b at 2026-06-08 00:27

--- abs/abc081b/main.cpp
+++ abs/abc081b/main.cpp
@@ -1,9 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int main() {
-    // Code here
-    return 0;
+    int N, count = 0, stop = 0, tmp = 0;
+    cin >> N;
+    vector<int> A(N);
+
+    rep(i, N) {
+        cin >> A[i];
+    }
+
+    for(int i = 0; stop < 1;i++ ) {
+        if (A[i] % 2 == 0) {
+            A[i] /= 2;
+            tmp++;
+            if (tmp == N) {
+                tmp = 0;
+            }
+        }
+        else {
+            stop++;
+        }
+        if (i == N - 1) {
+            i = 0;
+            count++;
+        }
+    }
+    cout << count <<  endl;
 }
