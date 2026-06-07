bd4375e510717e64da09bac96dfaaa740223ce3e
RI0123 <ishiura.ryusei@icloud.com>
2026-06-07 22:55:52 +0900

    Solved: abs_abc086a at 2026-06-07 22:55

--- abs/abc086a/main.cpp
+++ abs/abc086a/main.cpp
@@ -4,6 +4,13 @@ using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int main() {
-    // Code here
-    return 0;
+    int a, b;
+    cin >> a >> b;
+    
+    if ((a * b) % 2 == 0) {
+        cout << "Even" << endl;
+    }
+    else {
+        cout << "Odd" << endl;
+    }
 }
