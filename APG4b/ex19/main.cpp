c4c9cecdf2d6ae5d755854a84804a38a8688e905
RI0123 <ishiura.ryusei@icloud.com>
2026-06-08 18:09:11 +0900

    Solved: APG4b_ex19 at 2026-06-08 18:09

--- APG4b/ex19/main.cpp
+++ APG4b/ex19/main.cpp
@@ -1,9 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
+void saiten(vector<vector<int>> &A, int &correct_count, int &wrong_count) {
+  for(int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      if (A[i][j] != (i + 1) * (j + 1)) {
+        A[i][j] = (i + 1) * (j + 1);
+        wrong_count++;
+      }
+      else {
+        correct_count++;
+      }
+    }
+  }
+}
+
+
+// -------------------
+// ここから先は変更しない
+// -------------------
 int main() {
-    // Code here
-    return 0;
+  // A君の回答を受け取る
+    vector<vector<int>> A(9, vector<int>(9));
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cin >> A.at(i).at(j);
+        }
+    }
+
+  int correct_count = 0; // ここに正しい値のマスの個数を入れる
+  int wrong_count = 0;   // ここに誤った値のマスの個数を入れる
+
+
+    saiten(A, correct_count, wrong_count);
+
+    
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << A.at(i).at(j);
+            if (j < 8) cout << " ";
+            else cout << endl;
+        }
+    }
+    cout << correct_count << endl;
+    cout << wrong_count << endl;
 }
