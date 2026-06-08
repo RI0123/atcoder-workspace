2c481df67ee080a42224810132de3b69a0025aca
RI0123 <ishiura.ryusei@icloud.com>
2026-06-08 10:49:25 +0900

    Solved: APG4b_ex16 at 2026-06-08 10:49

--- APG4b/ex16/main.cpp
+++ APG4b/ex16/main.cpp
@@ -1,9 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int main() {
-    // Code here
-    return 0;
+    vector<int> data(5);
+    int yes = 0;
+    for (int i = 0; i < 5; i++) {
+        cin >> data.at(i);
+    }
+
+  // dataの中で隣り合う等しい要素が存在するなら"YES"を出力し、そうでなければ"NO"を出力する
+  // 1.ループを使わないで考える
+    // 1. data[0] を見て data[1] と同じかどうかみる
+    // 2. data[1] を見て data[2] と同じかどうかみる
+    // .....
+    // もし == なら breakする
+    rep(i, data.size()) {
+        if (data[i] == data[i + 1]) {
+            cout << "YES" << endl;
+            yes++;
+            break;
+        }
+    }
+    if (yes == 0) {
+        cout << "NO" << endl;
+    }
 }
