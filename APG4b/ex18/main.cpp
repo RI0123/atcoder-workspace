067c8b5e5cdd1ff10471295fc6f9a3355bb18cf5
RI0123 <ishiura.ryusei@icloud.com>
2026-06-08 16:04:39 +0900

    Solved: APG4b_ex18 at 2026-06-08 16:04

--- APG4b/ex18/main.cpp
+++ APG4b/ex18/main.cpp
@@ -1,13 +1,30 @@
-#include <bits/stdc++.h>
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
 
-int main() {
-    int N, M;
-    cin >> N >> M;
-    vector<int> A(M), B(M);
-    for (int i = 0; i < M; i++) {
-    cin >> A.at(i) >> B.at(i);
+    int main() {
+        int N, M;
+        cin >> N >> M;
+        vector<int> A(M), B(M);
+        for (int i = 0; i < M; i++) {
+        cin >> A.at(i) >> B.at(i);
     }
 
-    
+    vector<vector<char>> ans(N, vector<char>(N, '-'));
+
+    for ( int i = 0; i < M; i++) {
+        ans[A[i] -1][B[i] -1] = 'o';
+        ans[B[i] -1][A[i] -1] = 'x';
+    }
+
+    for (int i = 0; i < N ; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << ans[i][j];
+            if ( j == M - 1) {
+                cout << endl;
+            }
+            else {
+                cout << " ";
+            }
+        }
+    }
 }
