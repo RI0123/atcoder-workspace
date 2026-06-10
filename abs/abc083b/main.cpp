dac1589bf1851c6a8d03dac50b0ec7122a808543
RI0123 <ishiura.ryusei@icloud.com>
2026-06-10 17:47:09 +0900

    Solved: abs_abc083b at 2026-06-10 17:47

--- abs/abc083b/main.cpp
+++ abs/abc083b/main.cpp
@@ -3,7 +3,25 @@ using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
+int GetDigit(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
 int main() {
-    // Code here
-    return 0;
+    int N, A, B;
+    cin >> N >> A >>B;
+    int total = 0;
+
+    for (int i = 1; i <= N;i++) {
+        int sum = GetDigit(i);
+        if (sum >= A && sum <= B) {
+            total += i;
+        }
+    }
+    cout  << total << endl;
 }
