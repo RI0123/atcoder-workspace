24357b31954ebbc0f33c7eef04377bad31642995
RI0123 <ishiura.ryusei@icloud.com>
2026-06-10 22:37:56 +0900

    AC abc085c

--- abs/abc085c/main.cpp
+++ abs/abc085c/main.cpp
@@ -4,6 +4,21 @@ using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int main() {
-    // Code here
-    return 0;
+  int N, Y;
+  cin >> N >> Y;
+
+  int x = -1,y = -1,z = -1,total = 0;
+
+  for (int i = 0;i <= N;i++) {
+    for (int j = 0; i + j <= N; j++) {
+        int k = N - i - j;
+        total = 10000 * i + 5000 * j + 1000 * k;
+        if (total == Y) {
+            x = i;
+            y = j;
+            z = k;
+        }
+    }
+  }
+  cout << x << " " << y << " " << z << endl; 
 }
