6f85d27c1b325b2c916788ead328c04d812f6e38
RI0123 <ishiura.ryusei@icloud.com>
2026-06-10 21:01:55 +0900

    Solved: abs_abc085b at 2026-06-10 21:01

--- abs/abc085b/main.cpp
+++ abs/abc085b/main.cpp
@@ -4,6 +4,23 @@ using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int main() {
-    // Code here
-    return 0;
+    int n, cnt = 1;
+    cin >> n;
+    vector<int> d(n + 1);
+
+    rep(i, n) {
+        cin >> d[i];
+    }
+
+    sort(d.begin(),d.end());
+    reverse(d.begin(),d.end());
+
+    rep(i,n) {
+        if (d[i+1] != 0) {
+            if (d[i] != d[i+1]) {
+                cnt++;
+            }
+        }
+    }
+    cout << cnt << endl;
 }
