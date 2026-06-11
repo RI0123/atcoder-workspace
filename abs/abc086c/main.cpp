d81779854f75ee9a054e784ab63c6818e73e9456
RI0123 <ishiura.ryusei@icloud.com>
2026-06-11 16:21:10 +0900

    Solved: abs_abc086c at 2026-06-11 16:21

--- abs/abc086c/main.cpp
+++ abs/abc086c/main.cpp
@@ -11,21 +11,23 @@ int main() {
 
     for(int i = 1; i < N + 1; i++) {
         cin >> data[i][0] >> data[i][1] >> data[i][2];
-        // cout << data[i][0] << " " << data[i][1] << " " << data[i][2] << " : " << i << endl;
     }
 
     for (int i = 1; i <= N ; i++) {
-        if (data[i][0] % 2 == 1) {
-            if (std::abs(data[i][1] - data[i-1][1] + data[i][2] - data[i-1][2]) % 2 != 1) {
-                // 奇数回目の話で前回との差が偶数のとき : no
-                YN = false;
+        if (std::abs(data[i][1] - data[i-1][1] + data[i][2] - data[i-1][2]) <= std::abs(data[i][0] - data[i - 1][0])) {
+            if (data[i][0] % 2 == 1) {
+                if (std::abs(data[i][1] - data[i-1][1] + data[i][2] - data[i-1][2]) % 2 != 1) {
+                    YN = false;
+                }
+            }
+            else {
+                if (std::abs(data[i][1] - data[i-1][1] + data[i][2] - data[i-1][2]) % 2 != 1) {
+                    YN = false;
+                }
             }
         }
         else {
-            // 偶数回目の話で前回との差が奇数のとき : no
-            if (std::abs(data[i][1] - data[i-1][1] + data[i][2] - data[i-1][2]) % 2 != 1) {
-                YN = false;
-            }
+            YN = false;
         }
     }
     if (YN == false) cout << "No" << endl;
