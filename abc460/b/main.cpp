958a8e6f66672a9c0b4c0d17846f4ad0fec6f6ef
RI0123 <ishiura.ryusei@icloud.com>
2026-06-12 13:59:34 +0900

    abc460b

--- abc460/b/main.cpp
+++ abc460/b/main.cpp
@@ -3,7 +3,36 @@ using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
+ll square(ll x) {
+    return x * x;
+}
+
 int main() {
-    
-    return 0;
+    ll t, x = 0, y = 0, distance = 0;
+    cin >> t;
+    vector<vector<ll>> data(t, vector<ll>(6, 0));
+
+    for(int i = 0;i < t; i++) {
+        // X1 Y1 R1 X2 Y2 R2
+        cin >> data[i][0] >> data[i][1] >> data[i][2] >> data[i][3] >> data[i][4] >> data[i][5];
+
+        // cout << data[i][0] << data[i][1] << data[i][2] << data[i][3] << data[i][4] << data[i][5] << endl;
+
+        x = std::abs(data[i][0] - data[i][3]);
+        y = std::abs(data[i][1] - data[i][4]);
+
+        distance = square(x) + square(y);
+
+        if (square(data[i][2] - data[i][5]) <= distance) {
+            if(distance <= square(data[i][2] + data[i][5])) {
+                cout << "Yes" << endl;
+            }
+            else {
+                cout << "No" << endl;
+            }
+        }
+        else {
+            cout << "No" << endl;
+        }
+    }
 }
