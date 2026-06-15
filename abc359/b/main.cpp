2ed1a96c60a215926d4b69d11ef750e4a8f0fae5
RI0123 <ishiura.ryusei@icloud.com>
2026-06-15 11:51:21 +0900

    B

--- abc359/b/main.cpp
+++ abc359/b/main.cpp
@@ -7,5 +7,14 @@ int main() {
     int N;
     cin >> N;
 
+    vector<int> data(N*2);
+    rep(i, N*2) cin >> data[i];
 
+    int cnt = 0;
+    rep(i, N*2 -2) {
+        if(data[i] == data[i+2]) {
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
 }
